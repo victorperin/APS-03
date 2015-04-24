548eff3b2114dbdf1a86c7791543250f4c3e1d47
Victor Perin <me@victorperin.com>
2015-04-24 15:17:44 -0300

    Inserindo algumas informações úteis...

--- Sorts.java
+++ Sorts.java
@@ -19,6 +19,8 @@ public class Sorts{
   //não se esqueça do "public static!"m mude apenas o nome exemplo sort e o comentario dentro do método...
   public static ArrayList<Imagem> exemploSort(ArrayList<Imagem> imagens){ //retorna um ArrayList de Imagem, mas usa 'imagens' como parametro
     //algoritimo de ordenação aqui
+    imagens.get(0).getNome; //se você for ordenar por nome, use esse método, lembre-se de usar um for e ao invés de 0 use uma variável (lembre-se isso é igual a um array)
+    imagens.get(0).getTamanhoBytes; //se for usar para ordenar por tamanho de arquivo (o mais fácil)
     return imagens;
   }
 
