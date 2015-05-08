798662fd7a5c701244b7df754d25677309698724
Victor Perin <victor_perin@nerdhitech.com.br>
2015-05-08 01:10:41 -0300

    Só corrigindo a estética do programa... :poop:

--- Main.java
+++ Main.java
@@ -73,11 +73,11 @@ public class Main{
 		System.out.printf("Tempo gasto BubbleSort:\t\t\t%.9f segundos.\n",(System.nanoTime() - tempoInicio)/1000000000);
 		//bubble sort
 
-				//Ancor sort (método próprio)
-				tempoInicio = System.nanoTime();
-				salvarArquivo("AncorSort.txt",Sorts.ancorSort(imagens));
-				System.out.printf("Tempo gasto AncorSort:\t\t\t%.9f segundos.\n",(System.nanoTime() - tempoInicio)/1000000000);
-				//Ancor sort (método próprio)
+		//Ancor sort (método próprio)
+		tempoInicio = System.nanoTime();
+		salvarArquivo("AncorSort.txt",Sorts.ancorSort(imagens));
+		System.out.printf("Tempo gasto AncorSort:\t\t\t%.9f segundos.\n",(System.nanoTime() - tempoInicio)/1000000000);
+		//Ancor sort (método próprio)
 
 
 		//ordenações por nome da imagem
