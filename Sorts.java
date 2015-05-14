58305f94b670c80e8715784aa9c224eb91ffea7b
Victor Perin <me@victorperin.com>
2015-05-14 15:24:42 -0300

    Inserindo ordenação por data de modificação em todos os arquivos Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -15,6 +15,7 @@
  * int tipo
  *  - 1: tamanhoBytes
  *  - 2: Nome do Arquivo
+ *  - 3: Data de modificação
  ************************************************************************/
  import java.util.Arrays;
  import java.util.ArrayList;
@@ -26,6 +27,7 @@ public class Sorts{
     switch(tipoSort){
       case "tamanho": tipo = 1;break;
       case "nome": tipo = 2;break;
+      case "data": tipo = 3;break;
       default:tipo = 0;
     }
 
@@ -66,7 +68,8 @@ public class Sorts{
         for (int i = menor + 1; i < imagens.size(); i++){
           boolean checarMenor;
           if(tipo==2) checarMenor = checarSeArquivoEhAntes(imagens.get(menor),imagens.get(i));
-          else checarMenor = imagens.get(i).getTamanhoBytes() < imagens.get(menor).getTamanhoBytes();
+          else if(tipo==1) checarMenor = imagens.get(i).getTamanhoBytes() < imagens.get(menor).getTamanhoBytes();
+          else checarMenor = imagens.get(i).getDataModificacao() < imagens.get(menor).getDataModificacao();
           if(checarMenor){
               menor = i;
           }
@@ -100,9 +103,14 @@ public class Sorts{
           for (j = i; j >= gap && checarSeArquivoEhAntes(imagens.get(j - gap),tempVal); j -= gap){
             imagens.set(j, imagens.get(j - gap)); // trocando...
           }
-        }else{
+        }else if(tipo==1){
   			  // comparando os elementos por tamanho
   			  for (j = i; j >= gap && tempVal.getTamanhoBytes() < imagens.get(j - gap).getTamanhoBytes(); j -= gap){
+  				  imagens.set(j, imagens.get(j - gap)); // trocando...
+  			  }
+        }else{
+  			  // comparando os elementos por tamanho
+  			  for (j = i; j >= gap && tempVal.getDataModificacao() < imagens.get(j - gap).getDataModificacao(); j -= gap){
   				  imagens.set(j, imagens.get(j - gap)); // trocando...
   			  }
         }
@@ -123,7 +131,8 @@ public class Sorts{
             {
                 boolean checarMenor;
                 if(tipo==2) checarMenor = checarSeArquivoEhAntes(imagens.get(i),imagens.get(i + 1));
-                else checarMenor = imagens.get(i).getTamanhoBytes() > imagens.get(i + 1).getTamanhoBytes();
+                else if(tipo==1) checarMenor = imagens.get(i).getTamanhoBytes() > imagens.get(i + 1).getTamanhoBytes();
+                else checarMenor = imagens.get(i).getDataModificacao() > imagens.get(i + 1).getDataModificacao();
                 if (checarMenor)
                 {
                    aux = imagens.get(i);
@@ -144,7 +153,8 @@ public class Sorts{
       for (int i = menor + 1; i < imagens.size(); i++){
         boolean checarMenor;
         if(tipo==2) checarMenor = checarSeArquivoEhAntes(imagens.get(menor),imagens.get(i));
-        else checarMenor = imagens.get(i).getTamanhoBytes() < imagens.get(menor).getTamanhoBytes();
+        else if(tipo==1) checarMenor = imagens.get(i).getTamanhoBytes() < imagens.get(menor).getTamanhoBytes();
+        else checarMenor = imagens.get(i).getDataModificacao() < imagens.get(menor).getDataModificacao();
         if (checarMenor){
     		    menor = i;
         }
@@ -169,7 +179,8 @@ public class Sorts{
         for (int i = menor + 1; i < imagens.size(); i++){
           boolean checarMenor;
           if(tipo==2) checarMenor = checarSeArquivoEhAntes(imagens.get(menor),imagens.get(i));
-          else checarMenor = imagens.get(i).getTamanhoBytes() < imagens.get(menor).getTamanhoBytes();
+          else if(tipo==1) checarMenor = imagens.get(i).getTamanhoBytes() < imagens.get(menor).getTamanhoBytes();
+          else checarMenor = imagens.get(i).getDataModificacao() < imagens.get(menor).getDataModificacao();
           if (checarMenor){
               menor = i;
           }
